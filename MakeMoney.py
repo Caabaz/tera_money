07cc345bed0ce713746dc6e059a9c29ed60b3a71
Caabaz <ashil97@mail.ru>
2019-05-07 17:19:22 +0300

    Add simple generation Some move on GUI

--- MakeMoney.py
+++ MakeMoney.py
@@ -234,7 +234,7 @@ class ExampleApp(QWidget):
             self.iteration = 500
         else:
             self.iteration = 100
-        # -------------------------- Чекбокс для случайной итерации ---------------- # test
+
         if self.check.isChecked():
             self.iteration = 1
         for i in range(self.iteration):
